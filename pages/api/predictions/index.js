a452098aca9b4207818bde9c20bd277b2e13e6c3
Hash Milhan <hashirm@gmail.com>
2024-04-19 11:05:47 +0100

    added comments

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
   const prediction = await replicate.predictions.create({
     // Pinned to a specific version of Stable Diffusion
     // See https://replicate.com/stability-ai/sdxl
-    version: "8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f",
-    // version: "727e49a643e999d602a896c774a0658ffefea21465756a6ce24b7ea4165eba6a",
+    version: "8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f", // SDXL
+    // version: "727e49a643e999d602a896c774a0658ffefea21465756a6ce24b7ea4165eba6a", // ByteDance model
 
     // This is the text prompt that will be submitted by a form on the frontend
     // input: { prompt: req.body.prompt },
